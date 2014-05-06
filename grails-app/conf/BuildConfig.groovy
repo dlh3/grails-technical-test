3252f89b386740a811a3ee16e8585f6f1af7ccca
Dave Hughes <dlh3@sfu.ca>
2014-05-06 13:29:37 -0700

    Added spring-security-core plugin and generated security domain classes.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,7 +42,8 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
-
+		compile ":spring-security-core:2.0-RC2"
+		
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
